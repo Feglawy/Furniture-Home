97e95df507fb720bd8394baeb3bbb16996c31324
Hemdan47 <mohammedahmed98731@gmail.com>
2025-08-23 19:34:28 +0300

    initial project structure

--- src/main/java/com/egronx/furniturehome/FurnitureHomeApplication.java
+++ src/main/java/com/egronx/furniturehome/FurnitureHomeApplication.java
@@ -1,4 +1,4 @@
-package com.example.FurnitureHome;
+package com.egronx.furniturehome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FurnitureHomeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FurnitureHomeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FurnitureHomeApplication.class, args);
+    }
 
 }
--- src/test/java/com/egronx/furniturehome/FurnitureHomeApplicationTests.java
+++ src/test/java/com/egronx/furniturehome/FurnitureHomeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.FurnitureHome;
+package com.egronx.furniturehome;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FurnitureHomeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
