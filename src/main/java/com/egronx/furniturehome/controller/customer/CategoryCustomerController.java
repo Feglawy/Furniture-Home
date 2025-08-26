a44ee2f528d8d26235cc9d226f6c102d65a40f0c
omnjja <omniahesham910@gmail.com>
2025-08-27 01:33:26 +0300

    add CRUD on category

---
+++ src/main/java/com/egronx/furniturehome/controller/customer/CategoryCustomerController.java
@@ -0,0 +1,26 @@
+package com.egronx.furniturehome.controller.customer;
+
+import com.egronx.furniturehome.entity.Category;
+import com.egronx.furniturehome.service.CategoryService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+
+public class CategoryCustomerController {
+
+    CategoryService categoryService;
+    public CategoryCustomerController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    // list all categories
+    @GetMapping("/categories")
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+
+}
